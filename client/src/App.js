243c65075b1ded5588fb176df03e3e8d88cc3474
Cdtama7 <cdtama7@gmail.com>
2020-03-11 17:17:36 -0500

    Login, logout and register added. Haven't gotten the database to work with it yet.

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,54 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Container from "./components/Container"
+import Container from "./components/Container";
 import Fridge from "./pages/Fridge";
 import Recipes from "./pages/Recipes";
-import Login from "./pages/Login";
+import RegisterForm from "./components/registerForm";
+import Logout from "./components/logout";
+import LoginForm from "./components/loginForm";
 import Footer from "./components/Footer";
+import React, { Component } from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import NotFound from "./components/notFound";
+import NavBar from "./components/navBar";
 
-function App() {
-    return (
-      <Router>
-        <div>
-          <Container>
-            <Route exact path="/" component={Fridge} />
-            {/* <Route exact path="/login" component={Login} /> */}
-            <Route exact path="/fridge" component={Fridge} />
-            <Route exact path="/recipes" component={Recipes} />
-          </Container>
-          <Footer/>
-        </div>
-      </Router>
-    );
+import ProtectedRoute from "./components/common/protectedRoute";
+import auth from "./services/authService";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+
+class App extends Component {
+  state = {};
+
+  componentDidMount() {
+    const user = auth.getCurrentUser();
+    this.setState({ user });
   }
 
-  export default App;
+  render() {
+    const { user } = this.state;
+
+    return (
+      <React.Fragment>
+        <ToastContainer />
+        <NavBar user={user} />
+        <div>
+          <main className="container">
+            <Switch>
+              <Route path="/register" component={RegisterForm} />
+              <Route path="/login" component={LoginForm} />
+              <Route path="/logout" component={Logout} />
+              <ProtectedRoute path="/fridge" component={Fridge} />
+              <ProtectedRoute path="/recipes" component={Recipes} />
+              <Route path="/not-found" component={NotFound} />
+              <Route path="/" component={LoginForm} />
+              <Redirect to="/not-found" />
+            </Switch>
+          </main>
+          <Footer />
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
