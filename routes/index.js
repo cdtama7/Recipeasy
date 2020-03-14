4262f6f2701aeb469648397d4c4ea5c5d7b74716
Firuza Abramyan <firuzaa8@gmail.com>
2020-03-14 11:50:38 -0400

    proxy server

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 const path = require("path");
 const router = require("express").Router();
 const apiRoutes = require("./api");
-
+const proxy = require('express-http-proxy');
 
 router.use("/api", apiRoutes);
-
+router.use("/spoonacular", proxy("https://api.spoonacular.com"));
 
 router.use(function(req, res) {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
