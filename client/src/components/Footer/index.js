e4f0d0b7bd8bb30bd7bb593f297131e0772f1ca6
Carlos De Castro <54776105+cdtama7@users.noreply.github.com>
2020-03-17 19:13:05 -0500

    Update index.js

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,14 +1,12 @@
 import React from "react";
 import "./index.css";
 
-
 function Footer() {
-    return (
-      <footer className="footer">
-        <span>RecipEasy</span>
-      </footer>
-    );
-  }
-  
-  export default Footer;
-  
+  return (
+    <footer className="footer fixed-bottom">
+      <span>RecipEasy</span>
+    </footer>
+  );
+}
+
+export default Footer;
