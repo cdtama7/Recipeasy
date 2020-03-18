1e479dc4885f1aa15d9b0977feec6178173ce8ce
Carlos De Castro <54776105+cdtama7@users.noreply.github.com>
2020-03-17 20:02:25 -0500

    Update index.js

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,19 +1,13 @@
 import React from "react";
 import "./index.css";
-<<<<<<< HEAD
-=======
+import logo from "../../logo.png";
 
->>>>>>> e4f0d0b7bd8bb30bd7bb593f297131e0772f1ca6
 function Footer() {
   return (
     <footer className="footer fixed-bottom">
-      <span>RecipEasy</span>
+      <img src={logo} />
     </footer>
   );
 }
-<<<<<<< HEAD
-export default Footer;  
-=======
 
 export default Footer;
->>>>>>> e4f0d0b7bd8bb30bd7bb593f297131e0772f1ca6
